fe506c24bafca469846959aa2a88440a0fc26ce5
Gendos <striker2011@gmail.com>
2019-08-11 19:52:23 +0300

    119

--- models/galleries.go
+++ models/galleries.go
@@ -17,6 +17,28 @@ type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
 
+var _ GalleryDB = &GalleryGorm{}
+
+func (gg *GalleryGorm) Create(gallery *Gallery) error {
+	return gg.db.Create(gallery).Error
+}
+
 type GalleryGorm struct {
 	db *gorm.DB
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+type galleryService struct {
+	GalleryDB
+}
+
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{
+			&GalleryGorm{db},
+		},
+	}
 }
