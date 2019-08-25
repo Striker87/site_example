07f15f279b873f0d4c243d1107b535ed9c2448dc
Gendos <striker2011@gmail.com>
2019-08-26 02:44:56 +0300

    129

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	r.HandleFunc("/galleries/{:id:[0-9]+}", galleriesC.Show).Methods("GET").Name(controllers.ShowGallery)
 	r.HandleFunc("/galleries/{id:[0-9]+}/edit", requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET")
 	r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).Methods("POST")
+	r.HandleFunc("/galleries/{id:[0-9]+}/delete", requireUserMw.ApplyFn(galleriesC.Delete)).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
